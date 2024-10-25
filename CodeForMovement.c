7acaedbc4709253908cc0b7d7c1a7781b6f14238
RMJ318 <125172079+RMJ318@users.noreply.github.com>
2024-10-25 15:45:43 +0800

    Update CodeForMovement.c

--- CodeForMovement.c
+++ CodeForMovement.c
@@ -3,8 +3,20 @@
 MeDCMotor motor1(M1);  // Motor 1 connected to port M1
 MeDCMotor motor2(M2);  // Motor 2 connected to port M2
 
-long right_duration = 
-long left_duration = 
+unsigned long forward_interval = 
+unsigned long turn_interval = 
+unsigned long uTurn_interval = 
+unsigned long doubleTurn_interval =
+unsigned long nudge_interval = 
+unsigned long sampling_interval = 
+int colouroutput;
+int white_sample;
+int black_sample;
+int red_sample;
+int blue_sample;
+int green_sample;
+int orange_sample;
+ 
 
 void celebrate() {// Code for playing celebratory tune
   
@@ -14,56 +26,48 @@ void stopMotor() {// Code for stopping motor
   motor2.stop();
 }
 void moveForward() {// Code for moving forward for some short interval
-  for(long i = 0; i < 2; i ++)
   {
-    motor1.run(Forward_Left_Pins[i] , HIGH);
-    motor1.run(Backward_Left_Pins[i] , LOW);
-    motor1.run(Forward_Right_Pins[i] , HIGH);
-    motor1.run(Backward_Right_Pins[i] , LOW);
-  }
-
+    motor1.run(motor_speed);
+    motor2.run(motor_speed);
+    delay(forward_interval);
 }
 void turnRight() {// Code for turning right 90 deg
-  for(long i = 0; i < 2; i ++)
-  {
-    motor1.run(Forward_Left_Pins[i] , HIGH);
-    motor1.run(Backward_Left_Pins[i] , LOW);
-    motor1.run(Forward_Right_Pins[i] , LOW);
-    motor1.run(Backward_Right_Pins[i] , HIGH);
-  }
-  delay(right_duration)
+  motor1.run(motor_speed);
+  motor2.run(motor_speed_slower);
+  delay(turn_interval);
 }
 void turnLeft() {// Code for turning left 90 deg
-  for(long i = 0; i < 2; i ++)
-  {
-    motor1.run(Forward_Left_Pins[i] , LOW);
-    motor1.run(Backward_Left_Pins[i] , HIGH);
-    motor1.run(Forward_Right_Pins[i] , HIGH);
-    motor1.run(Backward_Right_Pins[i] , LOW);
-  }
-  delay(left_duration)
+  motor1.run(motor_speed_slower);
+  motor2.run(motor_speed);
+  delay(turn_interval);
 }
 void uTurn() {// Code for u-turn
-  turnLeft();
-  turnLeft();
+  motor1.run(motor_speed);
+  motor2.run(-motor_speed);
+  delay(uTurn_interval);
 }
 void doubleLeftTurn() {// Code for double left turn
   turnLeft();
   moveForward();
-  delay(doubleLeftTurn_interval);
+  delay(doubleTurn_interval);
   turnLeft();
 }
 void doubleRightTurn() {// Code for double right turn
   turnRight();
   moveForward();
-  delay(doubleLeftTurn_interval);
+  delay(doubleTurn_interval);
   turnRight();
 }
-void nudgeLeft() {// Code for nudging slightly to the left for some short interval
-  
+void nudgeLeft() {
+  motor1.run(motor_speed_slightly_slower);
+  motor2.run(motor_speed);
+  delay(nudge_interval);
 }
 void nudgeRight() {// Code for nudging slightly to the right for some short
-interval}
+  motor1.run(motor_speed);
+  motor2.run(motor_speed_slightly_slower);
+  delay(nudge_interval);
+}
 void shineIR() {// Code for turning on the IR emitter only}
 void shineRed() {// Code for turning on the red LED only}
 void shineGreen() {// Code for turning on the green LED only}
@@ -77,7 +81,12 @@ int detectColour()
 }
 void setup()
 {
-// Configure pinMode for A0, A1, A2, A3
+  //pinmode codes
+  white_sample = detect_colour();
+  delay(sampling_interval);
+  red_sample = detect_colour();
+  delay(sampling_interval);
+  
 }
 void loop()
 {
