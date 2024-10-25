03d06576bb71c6694ed529d9432b0ba2bcf231a6
gzc-ceg <E1481823@u.nus.edu>
2024-10-25 16:01:19 +0800

    Update CodeForMovement.c

--- CodeForMovement.c
+++ CodeForMovement.c
@@ -80,6 +80,7 @@ void shineGreen() {// Code for turning on the green LED only}
 void shineBlue() {// Code for turning on the blue LED only}
 int detectColour()
 {
+ int red_value, green_value, blue_value;
 // Shine Red, read LDR after some delay
 // Shine Green, read LDR after some delay
 // Shine Blue, read LDR after some delay
