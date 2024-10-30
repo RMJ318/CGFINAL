0adf9590b5f0414144990d038192063d12e45da1
JeanPerrierIII <161400216+JeanPerrierIII@users.noreply.github.com>
2024-10-30 15:01:30 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,15 +5,15 @@
 #include "MeOrion.h"
 
 // LED control pins, connected to 2 - 4 Decoder
+// inputs too
 #define DECODE_PIN0 = A0;
 #define DECODE_PIN1 = A1;
-
-MeLineFollower lineFinder(PORT_2);
-
-// input
 #define LDR_IN = A2;
 #define IR_IN = A3;
 
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
+MeLineFollower lineFinder(PORT_2);
 
 void setup()
 {
@@ -30,11 +30,14 @@ void loop()
   int sensorState = lineFinder.readSensors();
   if (sensorState != S1_OUT_S2_OUT) //line detected
   {
+    motor1.stop;
+    motor2.stop;
     // see colour
   }
   else
   {
     move_forward();
+    nudge();
   }
   //delay(10); bot might take too long figuring out everything above to wait
 }
