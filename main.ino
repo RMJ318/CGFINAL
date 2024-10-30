8e91c749890344182fc5a4f2ddf4bb6f5179efef
JeanPerrierIII <161400216+JeanPerrierIII@users.noreply.github.com>
2024-10-30 14:54:45 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,17 +5,20 @@
 #include "MeOrion.h"
 
 // LED control pins, connected to 2 - 4 Decoder
-#define LED_PIN0 = A0;
-#define LED_PIN1 = A1;
+#define DECODE_PIN0 = A0;
+#define DECODE_PIN1 = A1;
+
+MeLineFollower lineFinder(PORT_2);
 
 // input
 #define LDR_IN = A2;
 #define IR_IN = A3;
 
+
 void setup()
 {
-  pinMode(LED_PIN1, OUTPUT);
-  pinMode(LED_PIN2, OUTPUT);
+  pinMode(DECODE_PIN0, OUTPUT);
+  pinMode(DECODE_PIN1, OUTPUT);
   pinMode(LDR_IN, INPUT);
   pinMode(IR_IN, INPUT);
   Serial.begin(9600);
@@ -23,5 +26,15 @@ void setup()
 
 void loop()
 {
-  
+  //step 1: IR Line detector
+  int sensorState = lineFinder.readSensors();
+  if (sensorState != S1_OUT_S2_OUT) //line detected
+  {
+    // see colour
+  }
+  else
+  {
+    move_forward();
+  }
+  //delay(10); bot might take too long figuring out everything above to wait
 }
