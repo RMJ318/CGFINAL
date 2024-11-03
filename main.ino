19ab00d36a93ae057d21ad589e5b2bd4995752b0
JeanPerrierIII <161400216+JeanPerrierIII@users.noreply.github.com>
2024-11-04 01:59:56 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -13,65 +13,9 @@
 #define ULTRASOUND 12;
 #define TIMEOUT 2000;
 #define SPEED_OF_SOUND 343;
-#define BASELINE XXXX;
-#define C_FACTOR 0.5; // Correction factor for forward movement
-
-MeDCMotor motor1(M1);
-MeDCMotor motor2(M2);
-MeLineFollower lineFinder(PORT_2);
-
-// gets distance from ultrasonic sensor
-float ultrasound_dist()
-{
-  pinMode(ULTRASONIC, OUTPUT);
-  digitalWrite(ULTRASONIC, LOW); 
-  delayMicroseconds(2); 
-  digitalWrite(ULTRASONIC, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(ULTRASONIC, LOW);
-  
-  pinMode(ULTRASONIC, INPUT);
-  long duration = pulseIn(ULTRASONIC, HIGH, TIMEOUT); 
-  if (duration > 0) 
-  {
-    return (duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100); 
-  }
-  else
-  {
-    return -1;
-  }
-}
-
-float ir_dist() // ir distance reading is likely to be unreliable
-{
-  float ambient = analogRead(IR_IN); // ambient light level, subtract from recorded 
-  delayMicroseconds(10);
-  digitalWrite(DECODE_PIN0, HIGH);
-  digitalWrite(DECODE_PIN1, HIGH);
-  float ir_in = analogRead(IR_IN) - ambient;
-  // magic to linearise and convert ir reading to cm
-  return ir_in;
-}
-
-void move_forward()
-{
-  // proportional movement method
-  //step 1: get distance w/ ultrasound
-  if (ultrasound_dist > 0)
-  {
-    float offset_left = baseline_left / ultrasound_dist(); // >1 if too close to wall, <1 if too far from wall
-  }
-    // ir sensor as fallback
-  else
-  {
-    float offset_right = baseline_right - ir_dist();
-  }
-  // 
-  motor1.run(100 * offset_left) // run left wheel runs faster if close to left wall, and slower if far from left wall
-}
 
 float baseline_left;
-float baseline_right;
+float baseline_right; // used by move forward function
 
 void setup()
 {
@@ -81,6 +25,7 @@ void setup()
   pinMode(IR_IN, INPUT);
   baseline_left = ultrasound_dist();
   baseline_right = ir_dist();
+  calibration_setup();
   Serial.begin(9600);
 }
 
@@ -92,11 +37,30 @@ void loop()
   {
     motor1.stop;
     motor2.stop;
-    see_colour();
+    int colour = detectColour();
+    switch (colour) {
+    case 1: 
+      turnRight();  
+      break;
+    case 2:  
+      doubleRightTurn();  
+      break;
+    case 3:  
+      moveForward();  
+      break;
+    case 4:  
+      turnLeft(); 
+      break;
+    case 5:  
+      uTurn();  
+      break;
+    default:
+      stopMotor();
+    }
   }
   else
   {
-    move_forward();
+    moveForward();
+    delay(50); //delay 50ms
   }
-  //delay(10); bot might take too long figuring out everything above to wait
 }
