099222d556965168d6ed849d9f61c2134509e692
JeanPerrierIII <161400216+JeanPerrierIII@users.noreply.github.com>
2024-11-08 16:06:25 +0800

    Update Colour Sensor.ino

--- Colour Sensor.ino
+++ Colour Sensor.ino
@@ -2,18 +2,10 @@
 
 // decoder pins and LDR pin declared in main.ino
 
-#define RED_LED 0;
-#define GREEN_LED 1;
-#define BLUE_LED 2;
-#define IR 3
+
 #define RGBWait 200 
 #define LDRWait 10
 
-int red = 0;
-int green = 0;
-int blue = 0;
-
-float colourArray[] = {0,0,0}; //return to mBot movement
 int colourCode = 0;
 //RED == 1
 //ORANGE == 2
@@ -21,7 +13,6 @@ int colourCode = 0;
 //BLUE == 4 
 // PINK == 5
 
-char colourStr[3][5] = {"R = ", "G = ", "B = "};
 
 void shine_led(int i)
 {
@@ -31,41 +22,6 @@ void shine_led(int i)
   digitalWrite(DECODE_PIN1, i % 2);
 }
 
-
-int detectColour()
-{
-  int red_value, green_value, blue_value;
-
-  // Shine Red, read LDR after some delay
-  shine_led(RED_LED);
-  delay(100);
-  red_value = analogRead(LDR_pin);   
-
-  // Shine Green, read LDR after some delay
-  shine_led(GREEN_LED);
-  delay(100);
-  green_value = analogRead(LDR_pin);
-  
-  // Shine Blue, read LDR after some delay
- shine_led(BLUE_LED);
- delay(100);
- blue_value = analogRead(LDR_pin);   
-
-  
- // Run algorithm for colour decoding
- if (red_value > green_value && red_value > blue_value) {
-    return 1;
-  } else if (green_value > red_value && green_value > blue_value) {
-    return 3;
-  } else if (blue_value > red_value && blue_value > green_value) {
-    return 4;
-  } else if (red_value > blue_value && green_value > blue_value) {
-    return 2; 
-  } else {
-    return 5;
-  }
-}
-
 int detectColour_2() {
 detect();
 return assignCode();
@@ -73,46 +29,52 @@ return assignCode();
 
 void detect() {                        
 for(int c = 0; c < 3; c++) { // for each colour LED
+  // Serial.println("LED ON");
   shine_led(c); //turn ON selected LED
   delay(RGBWait);
   colourArray[c] = getAvgReading(5); //get ave reading of the colour the LDR is exposed to
   colourArray[c] = (colourArray[c] - blackArray[c])/(greyDiff[c])*255;
+  // Serial.println("LED OFF");
   shine_led(IR); //turn OFF selected LED
   delay(RGBWait);
  }
 }
 
 int assignCode(){
-if (         (colourArray[0] >= (redArray[0] - 20) && colourArray <= (redArray[0] + 20)) && 
-             (colourArray[1] >= (redArray[1] - 20) && colourArray <= (redArray[1] + 20)) &&
-             (colourArray[2] >= (redArray[2] - 20) && colourArray <= (redArray[2] + 20)) ){
-return 1; // RED
-} else if (  (colourArray[0] >= (orangeArray[0] - 20) && colourArray <= (orangeArray[0] + 20)) && 
-             (colourArray[1] >= (orangeArray[1] - 20) && colourArray <= (orangeArray[1] + 20)) &&
-             (colourArray[2] >= (orangeArray[2] - 20) && colourArray <= (orangeArray[2] + 20)) ){
-return 2; // ORANGE
-} else if (  (colourArray[0] >= (greenArray[0] - 20) && colourArray <= (greenArray[0] + 20)) && 
-             (colourArray[1] >= (greenArray[1] - 20) && colourArray <= (greenArray[1] + 20)) &&
-             (colourArray[2] >= (greenArray[2] - 20) && colourArray <= (greenArray[2] + 20)) ){
-return 3: // GREEN
-} else if (  (colourArray[0] >= (blueArray[0] - 20) && colourArray <= (blueArray[0] + 20)) && 
-             (colourArray[1] >= (blueArray[1] - 20) && colourArray <= (blueArray[1] + 20)) &&
-             (colourArray[2] >= (blueArray[2] - 20) && colourArray <= (blueArray[2] + 20)) ){
-return 4; // BLUE
-} else if (  (colourArray[0] >= (pinkArray[0] - 20) && colourArray <= (pinkArray[0] + 20)) && 
-             (colourArray[1] >= (pinkArray[1] - 20) && colourArray <= (pinkArray[1] + 20)) &&
-             (colourArray[2] >= (pinkArray[2] - 20) && colourArray <= (pinkArray[2] + 20)) ){
-return 5; // PINK
-}
+  for (int i = 0; i < 3; i++)
+  {
+    Serial.println(colourArray[i]);
+  }
+  if ((colourArray[0] >= (redArray[0] - 20) && colourArray[0] <= (redArray[0] + 20)) && 
+      (colourArray[1] >= (redArray[1] - 20) && colourArray[1] <= (redArray[1] + 20)) &&
+      (colourArray[2] >= (redArray[2] - 20) && colourArray[2] <= (redArray[2] + 20)))
+      {
+        return 1; // RED
+      }
+  if ((colourArray[0] >= (orangeArray[0] - 20) && colourArray[0] <= (orangeArray[0] + 20)) && 
+      (colourArray[1] >= (orangeArray[1] - 20) && colourArray[1] <= (orangeArray[1] + 20)) &&
+      (colourArray[2] >= (orangeArray[2] - 20) && colourArray[2] <= (orangeArray[2] + 20)))
+      {
+        return 2; // ORANGE
+      } 
+  if ((colourArray[0] >= (greenArray[0] - 20) && colourArray[0] <= (greenArray[0] + 20)) && 
+      (colourArray[1] >= (greenArray[1] - 20) && colourArray[1] <= (greenArray[1] + 20)) &&
+      (colourArray[2] >= (greenArray[2] - 20) && colourArray[2] <= (greenArray[2] + 20)))
+      {
+        return 3; // GREEN
+      } 
+  if ((colourArray[0] >= (blueArray[0] - 20) && colourArray[0] <= (blueArray[0] + 20)) && 
+      (colourArray[1] >= (blueArray[1] - 20) && colourArray[1] <= (blueArray[1] + 20)) &&
+      (colourArray[2] >= (blueArray[2] - 20) && colourArray[2] <= (blueArray[2] + 20)))
+      {
+        return 4; // BLUE
+      } 
+  if ((colourArray[0] >= (pinkArray[0] - 20) && colourArray[0] <= (pinkArray[0] + 20)) && 
+      (colourArray[1] >= (pinkArray[1] - 20) && colourArray[1] <= (pinkArray[1] + 20)) &&
+      (colourArray[2] >= (pinkArray[2] - 20) && colourArray[2] <= (pinkArray[2] + 20)))
+      {
+       return 5; // PINK
+      }
+return 0;
 }
 
-int getAvgReading(int times) {
-  int reading;
-  int total = 0;
-  for(int i = 0; i < times; i++) {
-    reading = analogRead(LDR);
-    total = reading + total;
-    delay(LDRWait);
-  }
-  return total / times;
-}
