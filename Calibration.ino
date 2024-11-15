0dc63b0920889696ef428528c1735592f821edf5
JeanPerrierIII <161400216+JeanPerrierIII@users.noreply.github.com>
2024-11-15 15:38:29 +0800

    Update Calibration.ino

--- Calibration.ino
+++ Calibration.ino
@@ -2,21 +2,26 @@
 #define LDRWait 10 
 //#define LED 13 idk where the check light indicator is
 
+
+/*
 float colourArray[] = {0,0,0};
 float whiteArray[] = {0,0,0};
 float blackArray[] = {0,0,0};
 float greyDiff[] = {0,0,0};
-float referenceColours[5][3];
-//RED == 0
-//ORANGE == 1
-//GREEN == 2
-//BLUE == 3
-// PINK == 4
+
+
+//float referenceColour[5][3];
+char *message[5] = {"Red", "Orange", "Green", "Blue", "Pink"};
+//RED == 1
+//ORANGE == 2
+//GREEN == 3
+//BLUE == 4 
+// PINK == 5
 
 void calibration_setup()
 {
   setBalance(); //calibration
-  setColour(); //reading for each colour
+  // setColour(); //reading for each colour
 }
 
 void setBalance() 
@@ -25,29 +30,43 @@ void setBalance()
   delay(5000); 
 	
   for(int i = 0; i<=2 ;i++) {
-	  shine_led(i); //turn ON selected LED
-	  delay(RGBWait);
-	  whiteArray[i] = getAvgReading(5);
-	  shine_led(IR); //turn OFF selected LED
-	  delay(RGBWait);
+    shine_led(i); //turn ON selected LED
+    delay(RGBWait);
+    whiteArray[i] = getAvgReading(5);
+    shine_led(IR); //turn OFF selected LED
+    delay(RGBWait);
  }
 
   Serial.println("Put Black Sample For Calibration ...");
   delay(5000);
 	
   for(int i = 0;i<=2;i++) {
-	  shine_led(i); //turn ON selected LED
-	  delay(RGBWait);
-	  blackArray[i] = getAvgReading(5); 
-	  shine_led(IR); //turn OFF selected LED
-	  delay(RGBWait);
-	  greyDiff[i] = whiteArray[i] - blackArray[i];
-  }
+    shine_led(i); //turn ON selected LED
+    delay(RGBWait);
+    blackArray[i] = getAvgReading(5); 
+    shine_led(IR); //turn OFF selected LED
+    delay(RGBWait);
+    greyDiff[i] = whiteArray[i] - blackArray[i];
+   }
+    Serial.println("whiteArray:");
+    Serial.println(whiteArray[0]);
+    Serial.println(whiteArray[1]);
+    Serial.println(whiteArray[2]);
+    Serial.println("greyDiff:");
+    Serial.println(greyDiff[0]);
+    Serial.println(greyDiff[1]);
+    Serial.println(greyDiff[2]);
+    Serial.println("blackArray:");
+    Serial.println(blackArray[0]);
+    Serial.println(blackArray[1]);
+    Serial.println(blackArray[2]);
 }
 
+/*
 void setColour()
 {
 	for (int i = 0; i < 5; i++) {
+    Serial.println(message[i]);
 		delay(5000); 
 		for(int j = 0; j < 3 ; j++) {
 			shine_led(j); 
@@ -64,7 +83,14 @@ void setColour()
 			Serial.println(referenceColour[i][j]);
 		}
 	}
+  Serial.println("whiteajdakeh");
+  Serial.println((whiteArray[0] - blackArray[0])/(greyDiff[0])*255);
+  Serial.println((whiteArray[1] - blackArray[1])/(greyDiff[1])*255);
+  Serial.println((whiteArray[2] - blackArray[2])/(greyDiff[2])*255);
 }
+
+*/
+
 
 int getAvgReading(int times) {
   int reading;
