84ce5ba3c824a77accd22cc750e1720cbb489a6b
JeanPerrierIII <161400216+JeanPerrierIII@users.noreply.github.com>
2024-11-15 15:38:53 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -6,16 +6,53 @@
 
 // LED control pins, connected to 2 - 4 Decoder
 // inputs too
-#define DECODE_PIN0 = A0;
-#define DECODE_PIN1 = A1;
-#define LDR_IN = A2;
-#define IR_IN = A3;
-#define ULTRASOUND 12;
-#define TIMEOUT 2000;
-#define SPEED_OF_SOUND 343;
+#define DECODE_PIN0 A0
+#define DECODE_PIN1 A1
+#define LDR_IN A2
+#define IR_IN A3
+#define ULTRASONIC 12
+#define TIMEOUT 1600
+#define SPEED_OF_SOUND 343
 
-float baseline_left;
-float baseline_right; // used by move forward function
+#define RED_LED 0
+#define GREEN_LED 1
+#define BLUE_LED 2
+#define IR 3
+#define SAMPLES 5
+
+
+MeDCMotor motor1(M1);  // Motor 1 connected to port M1
+MeDCMotor motor2(M2);  // Motor 2 connected to port M2
+MeLineFollower lineFinder(PORT_2); // Linefinder connected to port 2
+MeBuzzer buzzer;
+
+float baseline_left = 12;
+float baseline_right = 17; // used by move forward function
+
+//function prototypes
+
+// movement
+float ultrasound_dist();
+int read_ave(int pin, int k);
+float offset();
+float ir_dist();
+void stopMotor();
+void moveForward();
+void turnRight();
+void turnLeft();
+void uTurn();
+void doubleLeftTurn();
+void doubleRightTurn();
+
+// colour detection
+// void calibration_setup();
+int detectColour();
+int assignCode();
+void shine_led(int i);
+void detect();
+void setBalance();
+void setColour();
+int getAvgReading(int times);
 
 void setup()
 {
@@ -23,44 +60,60 @@ void setup()
   pinMode(DECODE_PIN1, OUTPUT);
   pinMode(LDR_IN, INPUT);
   pinMode(IR_IN, INPUT);
-  baseline_left = ultrasound_dist();
-  baseline_right = ir_dist();
-  calibration_setup();
+  shine_led(IR);
   Serial.begin(9600);
+  // calibration_setup();
 }
 
 void loop()
 {
-  //step 1: IR Line detector
   int sensorState = lineFinder.readSensors();
-  if (sensorState != S1_OUT_S2_OUT) //line detected
+  Serial.println(sensorState);
+  if (sensorState == 0) //line detected
   {
-    motor1.stop;
-    motor2.stop;
+    stopMotor();
+    moveBackabit();
     int colour = detectColour();
-    switch (colour) {
-    case 1: 
-      turnRight();  
-      break;
-    case 2:  
-      doubleRightTurn();  
-      break;
-    case 3:  
-      moveForward();  
-      break;
-    case 4:  
-      turnLeft(); 
-      break;
-    case 5:  
-      uTurn();  
-      break;
-    default:
-      stopMotor();
+    Serial.println("detectColour output:");
+    switch (colour) 
+    {
+      case 1: 
+        Serial.println("Red");
+        Serial.println("Turn Left");
+        turnLeft();
+        break;
+      case 2:  
+        Serial.println("Orange");
+        Serial.println("uTurn");
+        uTurn();
+        break;
+      case 3:  
+        Serial.println("Green");
+        Serial.println("Right Turn");
+        turnRight();
+        break;
+      case 4:  
+        Serial.println("Blue");
+        Serial.println("dTurn Right");
+        doubleRightTurn();
+        break;
+      case 5:  
+        Serial.println("Pink");
+        Serial.println("dLeft Turn");
+        doubleLeftTurn();
+        break;
+      default:
+        Serial.println("Stop");
+        stopMotor();
+        celebrate();
     }
+    delay(50);
   }
   else
   {
+    delay(50);
     moveForward();
-    delay(50); //delay 50ms
   }
+   //delay 50ms
 }
+
